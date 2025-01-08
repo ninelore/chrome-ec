fa945210610a5d6831f0b7b8a559f283d8bc798f
Keith Short <keithshort@chromium.org>
2025-01-08 14:46:50 -0700

    test: usbc: Add PDO selection tests
    Add test cases for the following PDO selection criteria
    * Pick best fixed, battery, or variable PDO
    * Reject augmented PDOs
    * Pick 5V/0A PDO when no other choices offered
    * Reject PDOs with 0 voltage
    * Pick highest wattage PDO
    
    BUG=none
    TEST=./twister -ivc -T zephyr/test/pdc
    
    Change-Id: I1ab1acdb4e54e0463e9cdc8168004fc7392d8c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6168595
    Tested-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/pdc/src/pdc_policy/select_best_pdo.c
+++ zephyr/test/pdc/src/pdc_policy/select_best_pdo.c
@@ -80,3 +80,175 @@ ZTEST(pd_select_best_pdo, test_fixed_only)
 			      test[i].max_mv, test[i].expected_index);
 	}
 }
+
+ZTEST(pd_select_best_pdo, test_board_values)
+{
+	LOG_INF("PD Power limits: %d mV, %d mA, %d mW",
+		CONFIG_PLATFORM_EC_USB_PD_MAX_VOLTAGE_MV,
+		CONFIG_PLATFORM_EC_USB_PD_MAX_CURRENT_MA,
+		CONFIG_PLATFORM_EC_USB_PD_MAX_POWER_MW);
+
+	/* This test suite assumes that the following board PD limits: */
+	zassert_equal(60000, CONFIG_PLATFORM_EC_USB_PD_MAX_POWER_MW);
+	zassert_equal(3000, CONFIG_PLATFORM_EC_USB_PD_MAX_CURRENT_MA);
+	zassert_equal(20000, CONFIG_PLATFORM_EC_USB_PD_MAX_VOLTAGE_MV);
+}
+
+ZTEST(pd_select_best_pdo, test_fixed_batt_var_pdo)
+{
+	/* The USB PD Spec indicates that the capabilities
+	 * message is sent in the following order:
+	 *   vSafe5V Fixed PDO
+	 *   Fixed PDOs (if present)
+	 *   Battery PDOs (if present)
+	 *   Variable PDOs (if present)
+	 */
+	const uint32_t partner_src_pdo[] = {
+		PDO_FIXED(5000, 500, PDO_FIXED_FLAGS),
+		PDO_FIXED(9000, 3000, PDO_FIXED_FLAGS),
+		PDO_FIXED(12000, 3000, PDO_FIXED_FLAGS),
+		PDO_BATT(14000, 18000, 36000),
+		PDO_VAR(19000, 20000, 3000),
+	};
+	uint32_t pdo;
+	uint32_t ma, max_mv, min_mv;
+
+	struct {
+		int max_mv;
+		int expected_index;
+	} test[] = {
+		{
+			.max_mv = 5000,
+			.expected_index = 0,
+		},
+		{
+			.max_mv = 6000,
+			.expected_index = 0,
+		},
+		{
+			.max_mv = 10000,
+			.expected_index = 1,
+		},
+		{
+			.max_mv = 12000,
+			.expected_index = 2,
+		},
+		{
+			.max_mv = 15000,
+			.expected_index = 2,
+		},
+		{
+			.max_mv = 18000,
+			.expected_index = 3,
+		},
+		{
+			.max_mv = 20000,
+			.expected_index = 4,
+		},
+	};
+
+	/* This test verifies non-fixed PDOs. */
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_ONLY_FIXED_PDOS)) {
+		ztest_test_skip();
+		return;
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(test); i++) {
+		LOG_INF("Find best PDO, max %d mV, expect index %d",
+			test[i].max_mv, test[i].expected_index);
+		zassert_equal(pd_select_best_pdo(ARRAY_SIZE(partner_src_pdo),
+						 partner_src_pdo,
+						 test[i].max_mv, &pdo),
+			      test[i].expected_index,
+			      "Max %d mV - failed to select fixed PDO index %d",
+			      test[i].max_mv, test[i].expected_index);
+
+		/* Additional check that the selected PDO's max
+		 * voltage doesn't exceed the configured maximum
+		 * voltage.
+		 */
+		pd_extract_pdo_power_unclamped(pdo, &ma, &max_mv, &min_mv);
+		zassert_true(max_mv <= test[i].max_mv);
+	}
+}
+
+ZTEST(pd_select_best_pdo, test_augmented_pdo)
+{
+	uint32_t partner_src_pdo[] = {
+		PDO_FIXED(5000, 500, 0),
+		PDO_AUG(5000, 20000, 3000),
+	};
+	uint32_t pdo;
+
+	/* The augmented PDO should be skipped and the vSafe5V fixed PDO
+	 * should be selected.
+	 */
+	zassert_equal(
+		pd_select_best_pdo(ARRAY_SIZE(partner_src_pdo), partner_src_pdo,
+				   CONFIG_USB_PD_MAX_VOLTAGE_MV, &pdo),
+		0, "Failed to select fixed PDO when augmented PDO present");
+}
+
+/* PD sources are permitted to offer no capabilities. In this case the source
+ * still provides a fixed PDO with the voltage set to 5V and the maximum
+ * current set to 0mA.
+ *
+ * Verify that the selection logic still picks the vSafe5V PDO.
+ */
+ZTEST(pd_select_best_pdo, test_fixed_5v_0a)
+{
+	const uint32_t partner_src_pdo[] = {
+		PDO_FIXED(5000, 0, PDO_FIXED_FLAGS),
+	};
+	uint32_t pdo;
+
+	zassert_equal(pd_select_best_pdo(ARRAY_SIZE(partner_src_pdo),
+					 partner_src_pdo,
+					 CONFIG_USB_PD_MAX_VOLTAGE_MV, &pdo),
+		      0, "Failed to select 5V/0mA fixed PDO");
+}
+
+ZTEST(pd_select_best_pdo, test_fixed_0v)
+{
+	const uint32_t partner_src_pdo[] = {
+		PDO_FIXED(5000, 500, PDO_FIXED_FLAGS),
+		PDO_FIXED(0, 3000, PDO_FIXED_FLAGS),
+		PDO_BATT(0, 0, 10000),
+		PDO_VAR(0, 0, 2000),
+		PDO_VAR(19000, 20000, 2000),
+	};
+	uint32_t pdo;
+	int expected_index;
+	int selected_index;
+
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_ONLY_FIXED_PDOS)) {
+		expected_index = 0;
+	} else {
+		expected_index = 4;
+	}
+	selected_index = pd_select_best_pdo(ARRAY_SIZE(partner_src_pdo),
+					    partner_src_pdo,
+					    CONFIG_USB_PD_MAX_VOLTAGE_MV, &pdo);
+	zassert_equal(
+		selected_index, expected_index,
+		"Incorrect PDO (%d) selected when 0V PDOs present, expected %d",
+		selected_index, expected_index);
+}
+
+ZTEST(pd_select_best_pdo, test_select_higher_wattage)
+{
+	uint32_t partner_src_pdo[] = {
+		PDO_FIXED(5000, 1500, 0),  PDO_FIXED(9000, 3000, 0),
+		PDO_FIXED(15000, 4000, 0), PDO_FIXED(20000, 4000, 0),
+		PDO_FIXED(21000, 5000, 0),
+	};
+	uint32_t pdo;
+
+	/* Should select PDO array index 3 despite having a higher wattage than
+	 * needed.
+	 */
+	zassert_equal(pd_select_best_pdo(ARRAY_SIZE(partner_src_pdo),
+					 partner_src_pdo,
+					 CONFIG_USB_PD_MAX_VOLTAGE_MV, &pdo),
+		      3, "Failed to select 20V/4A fixed PDO");
+}
