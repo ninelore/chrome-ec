82fc0f04c6d8d744145c53431b999700ac3d97c0
Jeremy Bettis <jbettis@google.com>
2025-01-10 10:04:05 -0700

    Remove unused keyboard mask macros
    And unneeded undefs.
    
    BUG=b:219051027
    TEST=CQ
    
    Change-Id: I787987d3f291af258bbfee8cb3a92e133a125519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6167687
    Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- include/keyboard_config.h
+++ include/keyboard_config.h
@@ -10,6 +10,8 @@
 
 #include "common.h"
 
+#define KEYBOARD_ROW_TO_MASK(r) (1 << (r))
+
 #ifdef CONFIG_KEYBOARD_CUSTOMIZATION
 /* include the board layer keyboard header file */
 #include "keyboard_customization.h"
@@ -42,42 +44,34 @@
 extern "C" {
 #endif
 
-#define KEYBOARD_ROW_TO_MASK(r) (1 << (r))
-
-/* Columns and masks for keys we particularly care about */
+/* Columns for keys we particularly care about when using a single keyboard
+ * layout. */
 #define KEYBOARD_COL_DOWN 11
 #define KEYBOARD_ROW_DOWN 6
-#define KEYBOARD_MASK_DOWN KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_DOWN)
 #define KEYBOARD_COL_ESC 1
 #define KEYBOARD_ROW_ESC 1
-#define KEYBOARD_MASK_ESC KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_ESC)
 #define KEYBOARD_COL_KEY_H 6
 #define KEYBOARD_ROW_KEY_H 1
-#define KEYBOARD_MASK_KEY_H KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_KEY_H)
 #ifdef CONFIG_KEYBOARD_STRAUSS
 #define KEYBOARD_COL_KEY_R 9
 #else
 #define KEYBOARD_COL_KEY_R 3
 #endif
 #define KEYBOARD_ROW_KEY_R 7
-#define KEYBOARD_MASK_KEY_R KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_KEY_R)
 #ifdef CONFIG_KEYBOARD_STRAUSS
 #define KEYBOARD_COL_LEFT_ALT 13
 #else
 #define KEYBOARD_COL_LEFT_ALT 10
 #endif
 #define KEYBOARD_ROW_LEFT_ALT 6
-#define KEYBOARD_MASK_LEFT_ALT KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_LEFT_ALT)
 #define KEYBOARD_COL_REFRESH 2
 #ifdef CONFIG_KEYBOARD_REFRESH_ROW3
 #define KEYBOARD_ROW_REFRESH 3
 #else
 #define KEYBOARD_ROW_REFRESH 2
 #endif
-#define KEYBOARD_MASK_REFRESH KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_REFRESH)
 #define KEYBOARD_COL_RIGHT_ALT 10
 #define KEYBOARD_ROW_RIGHT_ALT 0
-#define KEYBOARD_MASK_RIGHT_ALT KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_RIGHT_ALT)
 /*
  * Vol-up is part of vivaldi (a.k.a. top row keys). Thus, it needs to be
  * customized per boards.
@@ -90,7 +84,6 @@ extern "C" {
 #endif
 #define KEYBOARD_COL_LEFT_SHIFT 7
 #define KEYBOARD_ROW_LEFT_SHIFT 5
-#define KEYBOARD_MASK_LEFT_SHIFT KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_LEFT_SHIFT)
 
 #endif /* CONFIG_KEYBOARD_CUSTOMIZATION */
 
