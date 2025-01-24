d474b0e3820f74d08a3373f39f16921ff11520a8
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2025-01-24 23:04:03 +0800

    felino: modify power signal to boot S0
    Move power sequence config from ptl to felino, and modify some
    vw signals(S3/S4) to gpio signals to boot system to S0.
    
    BUG=b:388982526
    TEST=1. zmake build felino
         2. EC can boot to S0
    LOW_COVERAGE_REASON=b:380186099 initial bring-up
    
    Change-Id: I37fc5db1b7c73549507024765d39efb5b1f6c451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6198638
    Tested-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Dolan Liu <liuyong5@huaqin.corp-partner.google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>

--- zephyr/program/fatcat/felino/include/board.h
+++ zephyr/program/fatcat/felino/include/board.h
@@ -7,12 +7,12 @@
 #define FELINO_BOARD_H_
 
 /* Power Signals */
-#define PWR_EN_PP3300_S5 &gpioi 7
-#define PWR_EN_PP5000_S5 &gpiog 1
-#define PWR_RSMRST_PWRGD &gpiof 0
-#define PWR_EC_PCH_RSMRST &gpiob 2
+#define PWR_EN_PP3300_S5 &gpiof 2
+#define PWR_EN_PP5000_S5 &gpioh 3
+#define PWR_RSMRST_PWRGD &gpiog 1
+#define PWR_EC_PCH_RSMRST &gpioi 6
 #define PWR_SLP_S0 &gpioj 5
-#define PWR_PCH_PWROK &gpioj 1
-#define PWR_ALL_SYS_PWRGD &gpiod 5
+#define PWR_PCH_PWROK &gpiob 5
+#define PWR_ALL_SYS_PWRGD &gpioe 2
 
 #endif /* FELINO_BOARD_H_ */
