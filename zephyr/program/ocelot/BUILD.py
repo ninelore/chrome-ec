9716618e616cedda62b37528ca5792da85742445
Keith Short <keithshort@chromium.org>
2025-01-28 08:13:02 -0700

    ocelot: add initial ITE board
    Add a minimal config for the ocelot board based on the the ITE IT82002
    EC.
    
    BUG=b:383351974
    TEST=zmake build ocelot_ite
    TEST=Boot image on ITE AIC connected to Dagwood board
    
    Change-Id: Ia1ae86e1646f8a1769736d50e9db23d853b547fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6208289
    Tested-by: Keith Short <keithshort@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -27,6 +27,28 @@ def register_npcx9_project(
     )
 
 
+def register_it8xxx2_project(
+    project_name,
+    extra_kconfig_files=(),
+):
+    """Register an it8xxx2 based variant of ocelot."""
+    register_binman_project(
+        project_name=project_name,
+        zephyr_board="it8xxx2/it82002aw",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
+
+
 # For use on SKU1 and SKU2
 register_npcx9_project(
     project_name="ocelot_nuvoton",
@@ -35,6 +57,15 @@ register_npcx9_project(
     ],
 )
 
+# For use on SKU3
+register_it8xxx2_project(
+    project_name="ocelot_ite",
+    extra_kconfig_files=[
+        here / ".." / "intelrvp" / "zephyr_ap_pwrseq.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_nuvoton", addr=0x80144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocelot_ite", addr=0x60098)
