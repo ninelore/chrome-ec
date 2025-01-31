2a5e619310bccbc082f8bc74ee2f9501e28f2d0c
Tom Hughes <tomhughes@chromium.org>
2025-01-31 13:14:08 -0800

    util/check_zephyr_for_ec_symbols: Check for EC macros
    Add a way to check for EC macros that we don't want to use in Zephyr and
    suggest their Zephyr equivalent. This will make it easier to upstream
    code to Zephyr in the future.
    
    BUG=b:393593789
    TEST=./util/check_zephyr_for_ec_symbols.py zephyr/test/hwtest/src/timer.c
    TEST=./util/check_zephyr_for_ec_symbols.py --commit pre-submit
    
    Change-Id: I74593b0499f111ce48a759a72be930c097a444da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6221870
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Patryk Duda <patrykd@google.com>

--- util/check_zephyr_for_ec_symbols.py
+++ util/check_zephyr_for_ec_symbols.py
@@ -17,8 +17,20 @@ EC_FUNCTIONS: Dict[str, str] = {
     "udelay": "k_busy_wait",
 }
 
-EC_REGEXES: Dict[str, re.Pattern] = {
+EC_MACROS: Dict[str, str] = {
+    "MSEC": "USEC_PER_MSEC",
+    "SECOND": "USEC_PER_SEC",
+    "SEC_UL": "USEC_PER_SEC",
+    "MINUTE": "USEC_PER_SEC * SEC_PER_MIN",
+    "HOUR": "USEC_PER_SEC * SEC_PER_HOUR",
+}
+
+EC_FUNCTION_REGEXES: Dict[str, re.Pattern] = {
     func: re.compile(rf"\b{func}\b\(") for func in EC_FUNCTIONS
+}
+
+EC_MACRO_REGEXES: Dict[str, re.Pattern] = {
+    macro: re.compile(rf"\b{macro}\b") for macro in EC_MACROS
 }
 
 # Skip checking lines that have // NOLINT_EC_SYMBOL or /* NOLINT_EC_SYMBOL */ comments.
@@ -56,9 +68,19 @@ def main(argv: Optional[List[str]] = None) -> Optional[int]:
                 continue
 
             for func, replacement in EC_FUNCTIONS.items():
-                if EC_REGEXES[func].search(line):
+                if EC_FUNCTION_REGEXES[func].search(line):
                     print(
                         f"error: Function '{func}' should not be used. "
+                        f"Use '{replacement}' in Zephyr code.\n"
+                        f"{filename}:{linenum}: {line}",
+                        file=sys.stderr,
+                    )
+                    return_code = 1
+
+            for macro, replacement in EC_MACROS.items():
+                if EC_MACRO_REGEXES[macro].search(line):
+                    print(
+                        f"error: Macro '{macro}' should not be used. "
                         f"Use '{replacement}' in Zephyr code.\n"
                         f"{filename}:{linenum}: {line}",
                         file=sys.stderr,
