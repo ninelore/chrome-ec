7d1c8d102a09d0ce22850f14619ee9762dacb8dc
Tom Hughes <tomhughes@chromium.org>
2025-02-03 14:43:06 -0800

    zmake: add ptl-ish to ISH targets for compare-builds
    BUG=b:393439853
    TEST=zmake compare-builds ptl-ish
    
    Change-Id: I51919b859b7768ec5f290d6c7998969800ef3216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6228099
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Auto-Submit: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/zmake/zmake/compare_builds.py
+++ zephyr/zmake/zmake/compare_builds.py
@@ -285,7 +285,7 @@ class CompareBuilds:
             / pathlib.Path(project.config.project_name)
             / "output"
         )
-        ish_targets = {"rex-ish", "brox-ish", "orisa-ish"}
+        ish_targets = {"rex-ish", "brox-ish", "orisa-ish", "ptl-ish"}
 
         output_dir1 = self.checkouts[0].modules_dir / output_path
         output_dir2 = self.checkouts[1].modules_dir / output_path
