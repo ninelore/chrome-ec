ac035a2a36da887fa73d57e0d32f727762ff9537
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2025-02-06 16:10:27 +0800

    whitebeard: enable GMR sensor
    Enable using GMR sensor as a switch for entering the tablet mode.
    
    BUG=b:384579983
    TEST=make Board=whitebeard
    The GMR sensor is function normal.
    
    Change-Id: I2c9346c6100baa8e5797290d10f070b57317b101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6238129
    Commit-Queue: Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Reviewed-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Kenny Pan <kennypan@google.com>

--- board/hammer/variants.h
+++ board/hammer/variants.h
@@ -206,7 +206,8 @@
 /* GMR sensor for tablet mode detection */
 #if defined(BOARD_DON) || defined(BOARD_MASTERBALL) ||        \
 	defined(BOARD_MOONBALL) || defined(BOARD_WHISKERS) || \
-	defined(BOARD_EEL) || defined(BOARD_JEWEL)
+	defined(BOARD_EEL) || defined(BOARD_JEWEL) ||         \
+	defined(BOARD_WHITEBEARD)
 #define CONFIG_GMR_TABLET_MODE
 #endif
 
