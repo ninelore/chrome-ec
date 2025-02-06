50691a9d21db2f626a0242d699944e5efdbd1b83
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2025-02-06 16:11:34 +0800

    dirks: Remove support for charging/batt/bc1.2
    Dirks doesn't have battery, charger ic and bc1.2 ic. It only
    supports one usbc port and use ppc syv682x for C0. This CL
    remove batteries and charger configs and device trees. The
    unused i2c ports also are disabled.
    
    BUG=b:389391653
    TEST=1. zmake build dirks --clobber
         2. ./twister -T zephyr/test/nissa/ -v -i -s nissa.dirks
    
    Change-Id: I7c2e17bcb53791a6a4204708f54c663b8fb91e0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6238128
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>

---
+++ zephyr/program/nissa/dirks/include/board.h
@@ -0,0 +1,16 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Dirks declarations */
+
+#ifndef __CROS_EC_NISSA_DIRKS_BOARD_H__
+#define __CROS_EC_NISSA_DIRKS_BOARD_H__
+
+enum charge_port {
+	CHARGE_PORT_TYPEC0,
+	CHARGE_PORT_BARRELJACK,
+};
+
+#endif /* __CROS_EC_NISSA_DIRKS_BOARD_H__ */
