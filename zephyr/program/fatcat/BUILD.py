940fc4b352f32afdf1b0d3d3b239b39844c42a8b
Caveh Jalali <caveh@chromium.org>
2025-02-06 16:56:48 -0800

    fatcat_it82002aw: Enable PDC, charger, battery
    Enable:
      - PDC support
      - charger
      - battery
      - barrel jack
    
    based on ptlrvp configs.
    
    BUG=b:358181803
    TEST=booted fatcat_it82002aw to the OS
    
    Change-Id: I00222db4fc0241917f8d112ea44ce2310ef1aa57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6242775
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Tested-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- zephyr/program/fatcat/BUILD.py
+++ zephyr/program/fatcat/BUILD.py
@@ -61,6 +61,7 @@ register_it8xxx2_project(
     project_name="fatcat_it82002aw",
     extra_kconfig_files=[
         here / ".." / "intelrvp" / "zephyr_ap_pwrseq.conf",
+        here / ".." / "intelrvp" / "ptlrvp" / "pd.conf",
     ],
 )
 
