6e340c628388e25def7456e6b60fc9d040d7c3ef
Yuval Peress <peress@google.com>
2025-02-11 14:33:43 -0700

    npcx_monitor: Allow Zephyr toolchain hash as well
    Add a switch on which hash to compare against based on the
    ZEPHYR_TOOLCHAIN_VARIANT which will allow either Coreboot SDK or Zephyr
    (currently using 0.17.0, so additional hashes might need to be added
    later to support different version if needed). This is primarily
    intended for local builds and tests comparing the 2 toolchains.
    
    BUG=b:395569543
    TEST=zmake build -t zephyr trulo
    
    Change-Id: I9038c99d9a3424cc07dbf02c488b6090ed917803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6250704
    Tested-by: Yuval Peress <peress@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Auto-Submit: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Keith Short <keithshort@chromium.org>

--- zephyr/shim/chip/npcx/npcx_monitor/check_hash.cmake
+++ zephyr/shim/chip/npcx/npcx_monitor/check_hash.cmake
@@ -4,12 +4,20 @@
 
 file(SHA256 ${INPUT_FILE} npcx_monitor_hash)
 
-set(expected_hash "331629c64ef65caef1b23f0f29474f13d9b41447408a70b6e4260310801d324d")
+if ("${ZEPHYR_TOOLCHAIN_VARIANT}" STREQUAL "coreboot-sdk")
+  # Coreboot builds
+  set(expected_hash "331629c64ef65caef1b23f0f29474f13d9b41447408a70b6e4260310801d324d")
+elseif ("${ZEPHYR_TOOLCHAIN_VARIANT}" STREQUAL "zephyr")
+  # Zephyr builds
+  set(expected_hash "2a4abad66294d175ad82542f5be35af3802d9df5e12c58c9835a15e0886f78dd")
+endif()
 
 if("${npcx_monitor_hash}" STREQUAL "${expected_hash}")
   message(STATUS "NPCX Monitor hash match")
 else()
-  message(STATUS "NPCX monitor expected hash ${expected_hash}")
-  message(STATUS "NPCX monitor actual hash ${npcx_monitor_hash}")
-  message(FATAL_ERROR "NPCX monitor change detected. See README.md for instructions.")
+  message(FATAL_ERROR
+      "NPCX monitor expected hash ${expected_hash}\n"
+      "NPCX monitor actual hash ${npcx_monitor_hash}\n"
+      "NPCX monitor change detected. See README.md for instructions."
+  )
 endif()
