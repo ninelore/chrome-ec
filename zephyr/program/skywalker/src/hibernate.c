b3d9a38558701a5d9ed26bbcc7e105e867dfb874
Ting Shen <phoenixshen@google.com>
2025-02-17 17:53:00 +0800

    skywalker: limit EC control of en_pp5000_z1 to reference board only.
    In OEM projects, the en_pp5000_z1 signal will be directly controlled by
    hardware. This commit restricts the EC code that manages en_pp5000_z1 to
    the reference board.
    
    BUG=b:397139423
    TEST=zmake
    
    Change-Id: I0d4cf887c1066c3ecf971a9fd840d601d074cf11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6275806
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/program/skywalker/src/hibernate.c
+++ zephyr/program/skywalker/src/hibernate.c
@@ -10,8 +10,13 @@
 
 __override void board_hibernate_late(void)
 {
-	/* b/283037861: Pre-off the 5V power line for hibernate. */
+#ifdef CONFIG_BOARD_SKYWALKER
+	/* b/283037861, b/397139423:
+	 * Skywalker reference board only, Pre-off the 5V power line for
+	 * hibernate.
+	 */
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_z1), 0);
+#endif
 	/* It takes around 30ms to release the PP5000 capacitance. */
 	k_busy_wait(30 * USEC_PER_MSEC); /* 30ms */
 
